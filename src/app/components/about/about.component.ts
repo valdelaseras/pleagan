3942cfdf1bbe830b0b247b91a1419aba49f08351
Robin Scholtes <robin@theweirdscience.com>
2021-01-17 22:20:54 +1300

    some content and stuff

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AboutComponent {
+  section: string | null = null;
+  constructor( private route: ActivatedRoute ) {
+    this.route.queryParamMap.subscribe(
+      ( params: ParamMap ) => {
+        this.section = params.get( 'section' );
+      }
+    );
+  }
+  scrollTo(id: string): void {
+      setTimeout(() => {
+        const target = document.getElementById(id);
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+        }
+      }, 150);
+  }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  @Input() imageUrl = 'assets/icons/instagram.svg';
+  @Input() socialUrl = '/';
+  @Input() socialAlt = 'Instagram logo';
   constructor() {}
 
   ngOnInit(): void {}
