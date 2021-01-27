2ec837a57226663e79a83d73d4e11b77a0e1d412
Timothy Claassens <tim.claassens@gmail.com>
2021-01-27 15:39:11 +1300

    Finalised basic search implementation. Search endpoint has been incorporated in the app.

--- src/app/components/plea/children/pleas-list/pleas-list.component.ts
+++ src/app/components/plea/children/pleas-list/pleas-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Plea } from '../../../../model/plea';
 import { PleaService } from '../../../../services/plea.service';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { PLEA_STATUS } from 'pleagan-model';
 import {animate, stagger, style, transition, trigger, query } from '@angular/animations';
 
@@ -13,11 +13,6 @@ import {animate, stagger, style, transition, trigger, query } from '@angular/ani
   animations: [
     trigger('listAnimation', [
       transition('* => *', [
-        query(':leave', [
-          stagger(150, [
-            animate('0.8s', style({ opacity: 0 }))
-          ])
-        ], { optional: true }),
         query(':enter', [
           style({ opacity: 0 }),
           stagger(150, [
@@ -28,17 +23,17 @@ import {animate, stagger, style, transition, trigger, query } from '@angular/ani
     ])
   ],
 })
-export class PleasListComponent implements OnInit {
-  query: string;
+export class PleasListComponent {
+  updateQuery$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   pleas$: Observable<Plea[]>;
   constructor(private pleaService: PleaService) {
-    this.pleas$ = this.pleaService
-      .getPleas()
-      .pipe(map((pleas: Plea[]) => pleas.filter((plea: Plea) => plea.status !== PLEA_STATUS.COMPLIED)));
+    this.pleas$ = this.updateQuery$.pipe(
+      switchMap( ( query: string ) => query.length ? this.pleaService.searchPleas( query ) : this.pleaService.getPleas() ),
+      map((pleas: Plea[]) => pleas.filter((plea: Plea) => plea.status !== PLEA_STATUS.COMPLIED))
+    );
   }
 
-  ngOnInit(): void {}
-  updateQuery(query: string): void {
-    this.query = query;
+  updateQuery( query: string ): void {
+    this.updateQuery$.next( query );
   }
 }
