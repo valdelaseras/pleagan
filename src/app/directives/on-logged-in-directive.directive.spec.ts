c56a1d55802db133b3f4617ea366656cb2647c23
Robin Scholtes <robin@theweirdscience.com>
2021-02-16 20:08:58 +1300

    directive for when user is logged in/out

---
+++ src/app/directives/on-logged-in-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { onLoggedInDirective } from './on-logged-in.directive';
+
+describe('HideOnLogoutDirective', () => {
+  it('should create an instance', () => {
+    const directive = new onLoggedInDirective();
+    expect(directive).toBeTruthy();
+  });
+});
