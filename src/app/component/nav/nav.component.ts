7a0c0b9bacc10d95cf48340770f115179ac396de
Robin Scholtes <robin@theweirdscience.com>
2021-02-17 20:23:03 +1300

    fix logout navlink

--- src/app/component/nav/nav.component.ts
+++ src/app/component/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { AuthService } from '../../service/auth/auth.service';
+import {onLoggedInDirective} from '../../directives/on-logged-in.directive';
 
 @Component({
   selector: 'app-nav',
@@ -21,6 +22,7 @@ export class NavComponent {
     return window.innerWidth > 768;
   }
 
+  // TODO: onLoggedInDirective needs to be triggered again right after logout
   logout(): void {
     this.authService.logout();
   }
