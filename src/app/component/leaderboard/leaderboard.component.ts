b2301479aea6a9a4013d515c4d6bf476e05f5861
Robin Scholtes <robin@theweirdscience.com>
2021-02-21 01:45:33 +1300

    leaderboard basic idea, we should add some sorting options and some filters. Have users sort the leaderboard however they want

--- src/app/component/leaderboard/leaderboard.component.ts
+++ src/app/component/leaderboard/leaderboard.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Plea} from '../../model/plea';
+import {PleaService} from '../../service/plea/plea.service';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss'],
 })
-export class LeaderboardComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class LeaderboardComponent {
+  pleas$: Observable<Plea[]>;
+  constructor( private pleaService: PleaService ) {
+    this.pleas$ = pleaService.getPleas();
+  }
 }
