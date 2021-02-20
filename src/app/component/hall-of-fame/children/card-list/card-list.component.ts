30b9c196be7e01fdb44dc0835cb2aecd49f74006
Timothy Claassens <tim.claassens@gmail.com>
2021-02-21 00:20:43 +1300

    Fix hall of fame

--- src/app/component/hall-of-fame/children/card-list/card-list.component.ts
+++ src/app/component/hall-of-fame/children/card-list/card-list.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { PleaService } from '../../../../service/plea/plea.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Plea } from '../../../../model/plea';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { PLEA_STATUS } from 'pleagan-model';
+import { FADE_IN_LIST } from '../../../../animations';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.scss'],
+  animations: [FADE_IN_LIST],
 })
-export class CardListComponent implements OnInit {
-  query: string;
+export class CardListComponent {
+  updateQuery$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   pleas$: Observable<Plea[]>;
   constructor(private pleaService: PleaService) {
-    this.pleas$ = this.pleaService
-      .getPleas()
-      .pipe(map((pleas: Plea[]) => pleas.filter((plea: Plea) => plea.status === PLEA_STATUS.COMPLIED)));
+    this.pleas$ = this.updateQuery$.pipe(
+      switchMap((query: string) => (query.length ? this.pleaService.searchPleas(query) : this.pleaService.getPleas())),
+      map((pleas: Plea[]) => pleas.filter((plea: Plea) => plea.status === PLEA_STATUS.COMPLIED)),
+    );
   }
 
-  ngOnInit(): void {}
   updateQuery(query: string): void {
-    this.query = query;
+    this.updateQuery$.next(query);
   }
 }
