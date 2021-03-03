38b88dc0b29a255cd21121198155c428e8596e7e
Robin Scholtes <robin@theweirdscience.com>
2021-03-03 23:18:40 +1300

    modal on submit with a few options as reason to report

--- src/app/component/plea/plea-details/plea-details.component.ts
+++ src/app/component/plea/plea-details/plea-details.component.ts
@@ -3,25 +3,22 @@ import { Plea } from '../../../model/plea';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PleaService } from '../../../service/plea/plea.service';
-import { tap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-submission-details',
   templateUrl: './plea-details.component.html',
   styleUrls: ['./plea-details.component.scss'],
 })
 export class PleaDetailsComponent {
-  isVisible = false;
+  isOpen = false;
+  reportReason: string = 'inappropriate-content';
   plea$: Observable<Plea>;
   constructor(private route: ActivatedRoute, private pleaService: PleaService) {
     this.plea$ = this.pleaService.getPleaById(this.route.snapshot.paramMap.get('id') || '');
   }
-  scrollTo(id: string): void {
-    const target = document.getElementById(id);
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
-    }
-  }
-  showModal(): void {
-    this.isVisible = true;
+  submitReport(){
+    console.log('submit');
+    // todo: submit with reason value and plea id
+    this.isOpen = false;
   }
 }
