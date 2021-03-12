81aac1d88942ac3d161ec13a02061bd22a5d05c1
Timothy Claassens <tim.claassens@gmail.com>
2021-03-13 03:29:44 +1300

    Stuff 'n thangs

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://dev.pleagan.vg/api',
+  firebase: {
+    apiKey: "AIzaSyDf8yKIQEuiWhovIlZm7wBQhvAt88tktQs",
+    authDomain: "pleagan-dev.firebaseapp.com",
+    projectId: "pleagan-dev",
+    storageBucket: "pleagan-dev.appspot.com",
+    messagingSenderId: "968548444336",
+    appId: "1:968548444336:web:6cb3d971dc8984a44d3375",
+    measurementId: "G-4DHGLG82YP"
+  },
+};
