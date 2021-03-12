ca180f52c000fa48f8d9c2855de858fcb9c8b838
Robin Scholtes <robin@theweirdscience.com>
2021-03-13 02:22:29 +1300

    a really basic outline for profile page

--- src/app/module/user/user-routing.module.ts
+++ src/app/module/user/user-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SettingsComponent } from './component';
+import { ProfileComponent, SettingsComponent } from './component';
 
 const routes: Routes = [
   { path: 'settings', component: SettingsComponent, data: { animation: 'settings' } },
-
-  // @TODO: build a UserProfile component
-  // { path: ':userId', component: UserProfileComponent }
-  // { path: '', component: UserProfileComponent },
+  { path: 'profile', component: ProfileComponent, data: { animation: 'profile' } },
+  { path: ':pleaganDisplayName', component: ProfileComponent, data: { animation: 'profile' } },
 ];
 
 @NgModule({
