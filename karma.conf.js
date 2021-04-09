a71ddafb46f9abe97521ac42b27e65888083fa98
Timothy Claassens <tim.claassens@gmail.com>
2021-04-09 19:43:56 +1200

    Fixed replace string action that went wrong

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pleagan$'),
+      dir: require('path').join(__dirname, './coverage/pleagan'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   apiBaseUrl: '/api',
   firebase: {
     apiKey: "AIzaSyDWt1_1vmDfwxR6jCP1XI5V19DLbntiGXc",
-    authDomain: "pleagan$-c27f2.firebaseapp.com",
-    projectId: "pleagan$-c27f2",
-    storageBucket: "pleagan$-c27f2.appspot.com",
+    authDomain: "pleagan-c27f2.firebaseapp.com",
+    projectId: "pleagan-c27f2",
+    storageBucket: "pleagan-c27f2.appspot.com",
     messagingSenderId: "557713832714",
     appId: "1:557713832714:web:beefb0a7add043db4998f3",
     measurementId: "G-TLHR7KHTN8"
