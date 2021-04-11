7a4ea68fd1ef855870da34069bf61c79f756664e
Timothy Claassens <tim.claassens@gmail.com>
2021-04-11 22:30:04 +1200

    User is able to change their country of residence

--- src/app/module/user/component/settings/settings.component.ts
+++ src/app/module/user/component/settings/settings.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { AuthService, PleaganService } from '../../../core/service';
+import { AuthService, CountryService, PleaganService } from '../../../core/service';
 import { Pleagan } from '@shared/model';
 import { concat, forkJoin, Observable, of } from 'rxjs';
 import firebase from 'firebase/app';
 import { THEME } from 'pleagan-model';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { HTTP_LOADING_STATUS, HttpLoadingWrapper } from '@shared/model/http-loading-wrapper/http-loading-wrapper.model';
 import User = firebase.User;
 
@@ -17,6 +17,7 @@ export class SettingsComponent {
   themes = THEME;
   userRequest$: Observable<HttpLoadingWrapper<{auth: User, pleagan: Pleagan}>>;
   updateStatus: HTTP_LOADING_STATUS;
+  countries = CountryService.countries;
 
   constructor(
     private authService: AuthService,
@@ -35,7 +36,7 @@ export class SettingsComponent {
 
   saveUserSettings( user: { auth: User, pleagan: Pleagan } ): void {
     this.updateStatus = HTTP_LOADING_STATUS.LOADING;
-    this.pleaganService.updatePleagan( user.pleagan ).subscribe(() => this.updateStatus = HTTP_LOADING_STATUS.FINISHED);
+    this.pleaganService.updatePleagan( user.pleagan ).subscribe(() => this.updateStatus = HTTP_LOADING_STATUS.FINISHED );
   }
 
   confirmDeletion(): void {
